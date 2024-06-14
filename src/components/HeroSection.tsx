d23e125d2d55f90bc6a6324fbb9bd32a1ef3fb94
kerolos Magdy <kero.magdy2000@gmail.com>
2024-06-14 17:40:25 +0300

    feat: 🎸 HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,25 +1,51 @@
-import { Box, Typography } from '@mui/material';
 import { useEffect, useRef } from 'react';
-import * as THREE from 'three';
-import Vanta from 'vanta/dist/vanta.clouds.min';
+import Vanta from 'vanta/dist/vanta.birds.min';
+import Typed from "typed.js";
+import { Box, Typography, useTheme } from '@mui/material';
 
 export function HeroSection() {
+    const typedRef = useRef(null);
+    <Typography component='h4' variant='h4'></Typography>
+
+    useEffect(() => {
+        const typed = new Typed(typedRef.current, {
+            strings: ["Hello I'm Sara Ismail", "Hello I'm Data Analyst"],
+            typeSpeed: 50,
+            backSpeed: 50,
+            backDelay: 2000,
+            loop: true,
+            cursorChar: '',
+            smartBackspace: true,
+        });
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+
     const vantaRef = useRef(null);
 
     useEffect(() => {
         if (!vantaRef.current) {
             vantaRef.current = Vanta({
-                THREE: THREE,
                 el: "#home",
                 mouseControls: true,
                 touchControls: true,
                 gyroControls: false,
-                skyColor: 0x6dbbd9,
-                cloudColor: 0xb3c7e6,
-                cloudShadowColor: 0x1a3650,
-                sunColor: 0xff9e26,
-                sunGlareColor: 0xff6735,
-                sunlightColor: 0xff9326,
+                minHeight: 200.00,
+                minWidth: 200.00,
+                scale: 1.00,
+                scaleMobile: 1.00,
+                backgroundColor: 0x160625,
+                color1: 0xf5f5f5,
+                color2: 0xff000e,
+                colorMode: "lerp",
+                birdSize: 0.80,
+                wingSpan: 25.00,
+                speedLimit: 3.00,
+                separation: 75.00,
+                alignment: 50.00,
+                cohesion: 5.00,
             });
         }
 
@@ -30,12 +56,17 @@ export function HeroSection() {
         };
     }, []);
 
+    const theme = useTheme()
     return (
-        <Box id="home" sx={{ minHeight: '70vh', }}>
-            <Box className="header-content">
-                <Box className="intro-text">
-                    <Typography>Hi, I'm Sara</Typography>
-                </Box>
+        <Box id="home" sx={{ minHeight: '70vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: theme.spacing(2), maxWidth: '75%' }} gap={theme.spacing(1)}>
+                <Typography sx={{ textAlign: 'center', fontWeight: 'bold', fontSize: theme.spacing(4), color: theme.palette.primary.contrastText }} id="typed" ref={typedRef}></Typography>
+                <Typography sx={{ textAlign: 'center', color: theme.palette.grey[400], fontSize: theme.spacing(2) }}>
+                    Junior data analyst with a proven track record of
+                    leveraging data to address business challenges. Proficient in utilizing tools such as Power BI,
+                    Excel, MySQL, Python, and R to efficiently gather, clean, analyze, and visualize data. Demonstrated ability to translate complex datasets into
+                    actionable insights, contributing to informed decision-making. Keen attention to detail, strong analytical skills, and a commitment to delivering
+                    high-quality results. Seeking opportunities to apply my expertise in data analysis to drive strategic business outcomes.</Typography>
             </Box>
         </Box>
     );
