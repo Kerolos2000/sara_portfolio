2cad7b7b78db71c645420ab0d5d952fd8b3209f2
kerolos Magdy <kero.magdy2000@gmail.com>
2024-06-14 23:50:33 +0300

    feat: 🎸 update AppBar component

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,31 +1,170 @@
+import * as React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar as MuiAppBar, MenuItem, Toolbar, Container, IconButton, Menu, Box, Typography, styled, Switch, alpha, useTheme } from '@mui/material';
+import { useChangeMode } from '../hooks';
+import { Mode } from '../types';
+import { pascalCase } from 'change-case';
+import { useNavigate } from 'react-router-dom';
 
-import { AppBar as AppBarMui, Box, Container, Typography } from '@mui/material';
+export interface AppBarProps { }
 
-export function AppBar() {
+const pages = ['About Me', 'Experience', 'Skills', 'Projects', 'Contact'];
+
+export const AppBar: React.FC<AppBarProps> = () => {
+    const theme = useTheme();
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [prevScrollPos, setPrevScrollPos] = React.useState<number>(0);
+    const [visible, setVisible] = React.useState<boolean>(true);
+    const navigate = useNavigate();
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleClickMenu = (item: string) => {
+        navigate(`#${pascalCase(item)}`);
+        setAnchorElNav(null);
+    };
+
+    const [mode, setMode] = useChangeMode();
+
+    // Function to handle scroll events
+    const handleScroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        setVisible((prevScrollPos > currentScrollPos || currentScrollPos < 10) && currentScrollPos < 200);
+        setPrevScrollPos(currentScrollPos);
+    };
+
+    React.useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [prevScrollPos, visible]);
+
     return (
-        <AppBarMui sx={{ height: '3em' }} position="static">
-            <Container sx={{ height: '100%', alignItems: 'center', display: 'flex' }} maxWidth="xl">
-                <Typography
-                    variant="h6"
-                    noWrap
-                    component="a"
-                    href="#app-bar-with-responsive-menu"
-                    sx={{
-                        mr: 2,
-                        display: { xs: 'none', md: 'flex' },
-                        fontFamily: 'monospace',
-                        fontWeight: 700,
-                        letterSpacing: '.3rem',
-                        color: 'inherit',
-                        textDecoration: 'none',
-                    }}
-                >
-                    Sara
-                </Typography>
+        <MuiAppBar sx={{
+            top: visible ? 0 : -100, transition: 'top 0.5s ease-in-out', backdropFilter: 'blur(5px)',
+            background: alpha(theme.palette.background.paper, 0.5)
+        }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#HeroSection"
+                        sx={{
+                            mr: 2,
+                            display: 'flex',
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Sara
+                    </Typography>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <IconButton
+                        size="large"
+                        aria-label="menu"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleOpenNavMenu}
+                        color="inherit"
+                        edge="end"
+                        sx={{ display: { xs: 'block', md: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
 
-                <Box>
-                </Box>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={() => setAnchorElNav(null)}
+                        sx={{
+                            display: { xs: 'block', md: 'none' },
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <MenuItem key={page} onClick={() => handleClickMenu(page)}>
+                                {page}
+                            </MenuItem>
+                        ))}
+
+                        <MenuItem>
+                            <MaterialUISwitch sx={{ m: 1 }} checked={mode === Mode.dark} onChange={() => setMode(mode === Mode.dark ? Mode.light : Mode.dark)} />
+                        </MenuItem>
+                    </Menu>
+
+                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <MenuItem key={page} onClick={() => handleClickMenu(page)}>
+                                {page}
+                            </MenuItem>
+                        ))}
+                        <MaterialUISwitch sx={{ m: 1 }} checked={mode === Mode.dark} onChange={() => setMode(mode === Mode.dark ? Mode.light : Mode.dark)} />
+                    </Box>
+                </Toolbar>
             </Container>
-        </AppBarMui>
+        </MuiAppBar>
     );
 }
+
+
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+    width: 62,
+    height: 34,
+    padding: 7,
+    '& .MuiSwitch-switchBase': {
+        margin: 1,
+        padding: 0,
+        transform: 'translateX(6px)',
+        '&.Mui-checked': {
+            color: '#fff',
+            transform: 'translateX(22px)',
+            '& .MuiSwitch-thumb:before': {
+                backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+                    '#fff',
+                )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+            },
+            '& + .MuiSwitch-track': {
+                opacity: 1,
+                backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+            },
+        },
+    },
+    '& .MuiSwitch-thumb': {
+        backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+        width: 32,
+        height: 32,
+        '&::before': {
+            content: "''",
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            left: 0,
+            top: 0,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+                '#fff',
+            )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+        },
+    },
+    '& .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+        borderRadius: 20 / 2,
+    },
+}));
+
+
