cc1926cc1b0b0d4916f3b5e17103c9cb8d861ec2
kerolos Magdy <kero.magdy2000@gmail.com>
2024-06-14 23:54:32 +0300

    feat: 🎸 add base of AboutMeSection component

---
+++ src/components/AboutMeSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export interface AboutMeSectionProps { }
+
+export const AboutMeSection: React.FC<AboutMeSectionProps> = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,14 @@
+import { AboutMeSection } from "./AboutMeSection";
 import { AppBar } from "./AppBar";
 import { HeroSection } from "./HeroSection";
+export interface LayoutProps { }
 
-export function Layout() {
+export const Layout: React.FC<LayoutProps> = () => {
     return (
         <>
             <AppBar />
             <HeroSection />
+            <AboutMeSection />
         </>
     );
 }
